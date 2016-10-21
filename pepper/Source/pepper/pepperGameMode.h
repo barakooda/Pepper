5c6e29a5ac1fa4912d7c404d85ce5e3acc9c6905
BAMBALAPTOP\Barakooda <barak00@gmail.com>
2016-10-21 09:05:30 +0300

    starting game

---
+++ pepper/Source/pepper/pepperGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "pepperGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PEPPER_API ApepperGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
